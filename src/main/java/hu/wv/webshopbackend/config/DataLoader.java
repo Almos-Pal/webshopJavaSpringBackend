05fffd1797b0940f498c3d654ba8b34ca62aa9b5
Almos-Pal <pal.almos04@gmail.com>
2025-03-05 11:49:44 +0100

    feat: implement user seeding

--- src/main/java/hu/wv/webshopbackend/config/DataLoader.java
+++ src/main/java/hu/wv/webshopbackend/config/DataLoader.java
@@ -6,6 +6,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class DataLoader  implements CommandLineRunner {
 
@@ -23,18 +26,35 @@ public class DataLoader  implements CommandLineRunner {
     public void run(String... args) throws Exception {
          userRepository.deleteAll();
          userRepository.resetSequence();
-        User user = buildAdminUser();
-        userRepository.save(user);
+        User adminUser = buildAdminUser();
+        List<User> users = buildUsers();
+        userRepository.save(adminUser);
+        userRepository.saveAll(users);
     }
 
     private static User buildAdminUser() {
 
       return User.builder()
-              .username("admin")
+              .username("Admin")
               .admin(false)
               .password(passwordEncoder.encode("Password"))
-              .email("asd").build();
+              .admin(true)
+              .email("adm@adm.hu").build();
 
+
+    }
+
+    private static List<User> buildUsers() {
+        List<User> users = new ArrayList<>();
+        for(int i =0; i < 10; i++) {
+              users.add(  User.builder()
+                        .username("User" + i)
+                        .password(passwordEncoder.encode("Password" + i))
+                        .email("user" + i + "@gmail.com")
+                        .build());
+        }
+
+        return users;
 
     }
 }
